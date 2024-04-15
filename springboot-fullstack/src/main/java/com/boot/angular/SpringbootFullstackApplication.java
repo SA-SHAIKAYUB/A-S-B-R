2a5363356130df197468839d7b3ea96d47113634
SA-SHAIKAYUB <shaikayubsa5@gmail.com>
2024-04-15 14:53:24 -0700

    Initial commit

---
+++ springboot-fullstack/src/main/java/com/boot/angular/SpringbootFullstackApplication.java
@@ -0,0 +1,14 @@
+package com.boot.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFullstackApplication.class, args);
+		System.out.println("DEVELOPING FULL STACK DEVELOPMENT");
+	}
+
+}
---
+++ springboot-fullstack/src/test/java/com/boot/angular/SpringbootFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
